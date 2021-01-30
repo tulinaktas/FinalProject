4152e88baf22b3ffdb9fe4cd1e02bd13347923bc
Tülin Aktaş <tulin-aktas@hotmail.com>
2021-01-31 00:51:00 +0300

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,26 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        InMemoryProductDal _productDal;
+        public ProductManager(InMemoryProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+        public List<Product> GetAll()
+        {
+            //İs kodlari
+            //yetkisi var mi? 
+            //kontroller yapıldıktan sonra return
+            return _productDal.GetAll();
+
+        }
+    }
+}
---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    // bu bir database nesnesidir diye işaretlemek için
+    public interface IEntity
+    {
+    }
+}
